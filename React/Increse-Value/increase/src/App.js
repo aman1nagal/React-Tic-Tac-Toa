a6714fbf125094d773368286ac7b68a9e4450203
aman1nagal <amanagrawal427@gmail.com>
2022-09-28 15:44:10 +0530

    increase descreas button with reset

--- React/Increse-Value/increase/src/App.js
+++ React/Increse-Value/increase/src/App.js
@@ -1,12 +1,12 @@
 import React,{useState} from "react"
 const App=()=> {
- const [increase,setIncrease] =useState(0 )
+ const [increase,setIncrease] =useState(5)
   return (
 
     <div>
       <h1>{increase}</h1>
-      <button onClick={()=>{(setIncrease(increase>9?increase+1:""))}}>Increase</button>
-      <button onClick={()=>{(setIncrease(increase>0?increase-1:""))}}>Decrease</button>
+      <button onClick={()=>(increase<9?setIncrease(increase+1):"")}>Increase</button>
+      <button onClick={()=>(increase>0?setIncrease(increase-1):"")}>Decrease</button>
       <button onClick={()=>{setIncrease(0)}}>Reset</button>
     </div>
   );
